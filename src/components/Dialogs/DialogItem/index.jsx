9df6712accdd28d12eab24d7daaa0e9fbfe17716
Diana T <misoshmiso@gmail.com>
2025-03-03 14:49:51 +0300

    Added avatars to friends in messages

--- src/components/Dialogs/DialogItem/index.jsx
+++ src/components/Dialogs/DialogItem/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './index.module.css';
 import { NavLink } from 'react-router-dom';
+import { Avatar } from 'antd';
 
 const DialogItem = ({ dialog }) => {
 	return (
@@ -8,6 +9,11 @@ const DialogItem = ({ dialog }) => {
 			to={`/messages/${dialog.id}`}
 			className={styles.root}
 		>
+			<Avatar
+				className={styles.avatar}
+				size={110}
+				src={dialog.image}
+			/>
 			<div>
 				<span>{dialog.name}</span>
 			</div>
