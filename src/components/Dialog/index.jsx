cb69700a13b1c598f7a6bb082e345b0edcd09b7d
Diana T <misoshmiso@gmail.com>
2025-03-04 17:08:20 +0300

    Added back button

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import styles from './index.module.css';
 import { useParams } from 'react-router';
 import { dialogs } from '../../constants/constants';
+import { useNavigate } from 'react-router-dom';
+import { Button } from 'antd';
+import { CloseOutlined } from '@ant-design/icons';
 
 const Dialog = () => {
 	const { id } = useParams();
+	const navigate = useNavigate();
 	const targetDialog = dialogs.find((dialog) => dialog.id === id);
+
 	return (
 		<div className={styles.root}>
+			<Button
+				variant={'text'}
+				color={'default'}
+				icon={<CloseOutlined />}
+				onClick={() => navigate(-1)}
+			/>
 			<span>{targetDialog.lastMessage}</span>
 		</div>
 	);
