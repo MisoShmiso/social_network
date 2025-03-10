2506c2150cf173d33f7fae166dbb799ec50626f5
Diana T <misoshmiso@gmail.com>
2025-03-10 16:52:57 +0300

    Added input button

--- src/components/Feed/inputForPost/index.jsx
+++ src/components/Feed/inputForPost/index.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './index.module.css';
+import { Input } from 'antd';
+
+const { TextArea } = Input;
 
 const InputForPost = () => {
+	const [value, setValue] = useState();
+	const addPost = () => {
+		alert(value);
+	};
 	return (
 		<div className={styles.root}>
-		Posts input
+			<h3>My posts</h3>
+			<div>
+				<TextArea
+					value={value}
+					onChange={(e) => setValue(e.target.value)}
+					placeholder='Controlled autosize'
+					autoSize={{ minRows: 3, maxRows: 5 }}
+				/>
+			</div>
+			<div>
+				<button onClick={addPost}>Add post</button>
+			</div>
 		</div>
 	);
 };
-
 
 export default InputForPost;
